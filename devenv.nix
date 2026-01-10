9b8ab8a0467db31ca04e852dba44a6c8327485d5
Zeno Belli <xeno@os76.xyz>
2026-01-10 11:26:29 +0100

    chore: devenv init
    Init devenv, add git hooks

---
+++ devenv.nix
@@ -0,0 +1,53 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: {
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    commitizen
+    git
+  ];
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  # https://devenv.sh/basics/
+  enterShell = ''
+    hello         # Run scripts directly
+    git --version # Use packages
+  '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  git-hooks = {
+    excludes = [
+      "devenv.nix"
+    ];
+    hooks = {
+      alejandra.enable = true;
+      commitizen.enable = true;
+      comrak.enable = true;
+      deadnix.enable = true;
+      detect-aws-credentials.enable = true;
+      detect-private-keys.enable = true;
+      flake-checker.enable = true;
+      gitlint.enable = true;
+      selene.enable = true;
+      shellcheck.enable = true;
+      trufflehog.enable = true;
+    };
+  };
+}
